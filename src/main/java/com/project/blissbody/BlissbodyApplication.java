925d7063610a08b503e3e7e674d96d0092f31fb3
T-Pushyamitra <thamminenipushyamitra@gmail.com>
2024-06-24 08:51:30 +0530

    init

---
+++ src/main/java/com/project/blissbody/BlissbodyApplication.java
@@ -0,0 +1,13 @@
+package com.project.blissbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlissbodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlissbodyApplication.class, args);
+	}
+
+}
